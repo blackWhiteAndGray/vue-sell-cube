28f353e5cb52899ee195a65c5caf9a00e987fa30
pangfeng <pangfeng22@gmail.com>
2020-03-17 10:13:58 +0800

    商品详情开发

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,18 @@
   <div id="app"
        @touchmove.prevent>
     <v-header :seller="seller"></v-header>
+    <div class="tab">
+      <div class="tab-item border-1px">
+        <router-link to="/goods">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评价</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>
+    </div>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -32,4 +44,24 @@
   }
 </script>
 
-<style lang="stylus" scoped></style>
+<style lang="stylus" scoped>
+  @import "common/stylus/mixin.styl"
+
+  #app
+    .tab
+      display: flex
+      width: 100%
+      height: 40px
+      line-height: 40px
+      border-1px: rgba(7, 17, 27, 0.1)
+    .tab-item
+      flex: 1
+      text-align: center
+      & > a
+        display: block
+        text-decoration: none
+        font-size: 14px
+        color: rgb(77, 85, 93)
+        &.active
+          color: rgb(240, 20, 20)
+</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,28 @@
 import Vue from 'vue'
 import './cube-ui'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Goods from 'components/goods/goods'
+import Ratings from 'components/ratings/ratings'
+import Seller from 'components/seller/seller'
+import 'common/stylus/index.styl'
+
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/goods', component: Goods },
+  { path: '/ratings', component: Ratings },
+  { path: '/seller', component: Seller }
+]
+
+const router = new VueRouter({
+  routes,
+  linkActiveClass: 'active'
+})
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
